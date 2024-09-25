05c7bb05db7cd9dac00b1b18756baada2d29d015
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-09-25 11:51:45 -0300

    Craindo header para página não autenticada

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
+import 'bootstrap/dist/css/bootstrap.css';
 import styles from "@/styles/HomeNoAuth.module.scss"
+import HeaderNoAuth from "@/src/components/HomeNoAuth/HeaderNoAuth";
+
 
 
 export default function HomeNoAuth() {
@@ -12,7 +15,8 @@ export default function HomeNoAuth() {
 				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil." />
       </Head>
       <main>
-        <h1>Onebitflix</h1></main> 
+        <HeaderNoAuth></HeaderNoAuth>
+      </main> 
     </>
   );
 }
