b7fcc413c0b1c1ef8ee34becf4a9b0e4573b52c2
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-09-25 10:57:37 -0300

    Inserindo meta dados para o SEO. E criando estrutura de diretório para o os códigos do projeto.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,10 @@ export default function HomeNoAuth() {
   return (
     <>
       <Head>
-      <title>OneBitFlix</title>
+        <title>OneBitFlix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+			  <meta property="og:title" content="Onebitflix" key="title" />
+				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil." />
       </Head>
       <main>
         <h1>Onebitflix</h1></main> 
