743649027dd103c996967ca52008832bd88a6a0c
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-09-26 16:45:19 -0300

    Criação de componentes de slide para os cursos

---
+++ src/components/common/slideComponent/index.tsx
@@ -0,0 +1,31 @@
+import { Splide, SplideSlide } from 'splidejs/react-splide';
+import "@splidejs/splide/dist/css/splide.min.css";
+import { CourseType } from '@/src/services/courseServices';
+import SlideCard from '../slideCard';
+
+interface props {
+    courses: CourseType[];
+}
+
+const SlideComponent = function ({ courses }: props) {
+    return (
+        <>
+            <div>
+                <Splide options={{
+                        type: "loop",
+                        perPage: 4,
+                        perMove: 1,
+                        pagination: false,
+                }} >
+                     {courses?.map((course : CourseType) => (
+                        <SplideSlide key={course.id}>
+                        <SlideCard course={course} />
+                        </SplideSlide>
+                    ))}
+                </Splide>
+            </div>
+        </>
+    )
+  };
+  
+  export default SlideComponent;
