240140997b1718122dffbfc4c17bca4bb050c7f5
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-10-07 18:24:30 -0300

    Adicionando formulário de atualização de senha.

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -4,8 +4,11 @@ import UserForm from "@/src/components/profile/userForm";
 import { Button, Col, Container, Row } from "reactstrap";
 import HeaderAuth from "@/src/components/common/headerAuth";
 import Footer from "@/src/components/common/footerComponent";
+import { useState } from "react";
+import PasswordForm from "@/src/components/profile/passwordForm";
 
 const UserInfo = function () {
+  const [form, setForm] = useState("userForm");
   return (
     <>
       <Head>
@@ -20,15 +23,25 @@ const UserInfo = function () {
             <p className={styles.title}>Minha Conta</p>
             <Row className="pt-3 pb-5">
                 <Col md={4} className={styles.btnColumn}>
-                    <Button outline className={styles.renderFormBtn}>
-                        DADOS PESSOAIS
-                    </Button>
-                    <Button outline className={styles.renderFormBtn}>
-                        SENHA
-                    </Button>
+                  <Button
+                    outline
+                    className={styles.renderFormBtn}
+                    style={{ color: form === "userForm" ? "#ff0044" : "white" }}
+                    onClick={() => {setForm("userForm")}}
+                  >
+                    DADOS PESSOAIS
+                  </Button>
+                  <Button
+                    outline
+                    className={styles.renderFormBtn}
+                    style={{ color: form === "passwordForm" ? "#ff0044" : "white" }}
+                    onClick={() => {setForm("passwordForm")}}
+                  >
+                    SENHA
+                  </Button>
                 </Col>
                 <Col md>
-                    <UserForm />
+                  {form === "userForm" ? <UserForm /> : <PasswordForm />}
                 </Col>
             </Row>
         </Container>
